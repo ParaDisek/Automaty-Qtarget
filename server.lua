499ad167f72015af89592a918ff95626ea6c71af
ParaDisek <hitman92b@gmail.com>
2021-12-20 21:20:30 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,14 +5,13 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('Infram:BuySomething')
 AddEventHandler('Infram:BuySomething', function(item, cena)
-    print(item,cena)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     print('giveitem')
     if xPlayer.getMoney() >= cena then
          xPlayer.addInventoryItem(item,1)
     elseif xPlayer.getMoney() < cena then
-        print("brak wystarczającej ilości gotówki")
+        --print("brak wystarczającej ilości gotówki")
     end
     -- zróbcie jeszcze zabezpieczenie na wagę/limity itemów
 end)
